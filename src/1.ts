18db012f31849e10b516061c4c8763dd283e1b06
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2024-02-14 17:42:11 +0200

    Initial commit

---
+++ src/1.ts
@@ -0,0 +1,22 @@
+class Student {
+  constructor(public name: string, public age: number, public grade: string) {}
+}
+console.log(new Student("Bob", 25, "A"));
+
+export { Student };
+
+/*  Клас Student, який містить три властивості: name, age та grade. 
+  Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, напишіть скорочену ініціалізацію.
+
+class Student {
+  public name: string;
+  public age: number;
+  public grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+}
+*/
